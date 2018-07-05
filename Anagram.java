4945adb704ffe1443fb5904e411dda46f8f7d155
Gulshan Singh <gulshan.singh@st.niituniversity.in>
2018-07-05 12:23:19 +0530

    O(n) solution for Anagram-detection problem

---
+++ Anagram.java
@@ -0,0 +1,35 @@
+
+public class Anagram {
+
+	public static void main(String[] args) {
+		System.out.println(anagram("heart", "earth"));
+	}
+	
+	static Boolean anagram(String str1, String str2) {
+		
+		int[] c1 = new int[26];
+		int[] c2 = new int[26];
+		
+		for(int i = 0; i < str1.length(); i++) {
+			int pos = (int)(str1.charAt(i)) - 97;
+			c1[pos] = c1[pos]  + 1;
+		}
+		
+		for(int i = 0; i < str2.length(); i++) {
+			int pos = (int)(str2.charAt(i)) - 97;
+			c2[pos] = c2[pos]  + 1;
+		}
+		
+		int j = 0;
+		Boolean ok = true;
+		
+		while(j<26 && ok) {
+			if (c1[j] == c2[j]) {
+				j = j+1;
+			}
+			else
+				ok = false;
+		}
+		return ok;
+	}
+}
