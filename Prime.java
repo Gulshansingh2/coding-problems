594d15493ab93e985da1daffa99e3a10eb95addc
Gulshan Singh <gulshan.singh@st.niituniversity.in>
2018-07-09 21:51:49 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Prime {
+	
+	static boolean isPrime(int n)
+	{
+		if (n == 2) return true;
+
+		if (n <= 1 || n % 2 == 0 || n % 3 == 0)
+		    return false;
+	
+		for (int i = 5; i * i <= n; i = i + 6)
+			if (n % i == 0 || n % (i + 2) == 0)
+			return false;
+	
+		return true;
+	}
+
+
+	public static void main(String args[])
+	{
+		System.out.print("Enter any no. to check: ");
+		System.out.println();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(isPrime(n));
+	}
+}
