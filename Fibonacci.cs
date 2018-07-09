9576647631deefe0de5202c842f3572c9d3a07f9
Gulshan Singh <gulshan.singh@st.niituniversity.in>
2018-07-09 21:23:09 +0530

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace CodingProblems
+{
+    class Fibonacci
+    {
+        public int fib(int n)
+        {
+            int[] f = new int[n + 2];
+            int i;
+
+            f[0] = 0;
+            f[1] = 1;
+
+            Console.Write(f[0] + " " + f[1] + " ");
+
+            for(i = 2; i <= n; i++)
+            {
+                f[i] = f[i - 1] + f[i - 2];
+                Console.Write(f[i] + " ");
+            }
+
+            return f[n];
+       }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter any number: ");
+            new Fibonacci().fib(int.Parse(Console.ReadLine()));
+            Console.Read();
+        }
+    }
+}
