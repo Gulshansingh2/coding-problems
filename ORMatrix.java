44a56150b09498846273a47e8b9ecd9984de21b6
Gulshan Singh <gulshan.singh@st.niituniversity.in>
2018-07-23 14:50:21 +0530

    Comments

--- ORMatrix.java
+++ ORMatrix.java
@@ -4,25 +4,39 @@ import java.awt.List;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/*
+You are given a matrix of integers A with N rows (numbered 1 through N) and M columns (numbered 1 through M). Each element of this matrix is either 0 or 1.
+
+A move consists of the following steps:
+
+Choose two different rows r1 and r2 or two different columns c1 and c2.
+Apply the bitwise OR operation with the second row/column on the first row/column. Formally, if you chose two rows, this means you should change Ar1,k to Ar1,k∨Ar2,k for each 1≤k≤M; if you chose two columns, then you should change Ak,c1 to Ak,c1∨Ak,c2 for each 1≤k≤N.
+For each element of the matrix, compute the minimum number of moves required to make it equal to 1 or determine that it is impossible. Note that these answers are independent, i.e. we are starting with the initial matrix for each of them.
+*/
+
 public class ORMatrix {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		int n = sc.nextInt(); //no. of test cases
 		sc.nextLine();
-		String[] res = new String[n];
+		String[] res = new String[n]; //resultant array
 		
 		for(int j = 0; j<n; j++) {
-		
+			//space separated dimensions of array
 			String dim = sc.nextLine();
 			String[] dimArr = dim.split(" ");
-			int x = Integer.parseInt(dimArr[0]);
-			int y = Integer.parseInt(dimArr[1]);
+			int x = Integer.parseInt(dimArr[0]); // x dimension
+			int y = Integer.parseInt(dimArr[1]); //y dimension
 			
+			//input array
 			int[][] arr = new int[x][y];
+			//resultant array of particular input
 			int[][] arrRes = new int[x][y];
 			
+			//flag to check if array contains 1
 			boolean f= false;
+			//take input
 			for(int i = 0; i<x; i++) {
 				String val  = sc.nextLine();
 				String[] valArr = val.split(""); 
@@ -37,6 +51,7 @@ public class ORMatrix {
 					for(int k=0; k<y; k++) {
 						boolean flag = false;
 						if(arr[i][k] == 0) {
+							//check for 1 in the same column
 							for(int p=0; p<x; p++) {
 								if(arr[p][k] == 1) {
 									arrRes[i][k] = 1;
@@ -44,6 +59,7 @@ public class ORMatrix {
 								}
 							}
 							
+							//check for 1 in the same row
 							if(flag == false)
 								for(int p=0; p<y; p++) {
 									if(arr[i][p] == 1) {
@@ -60,6 +76,7 @@ public class ORMatrix {
 					}
 				}
 				
+			// if input array does not contain 1
 			else{
 				for(int i = 0; i<x; i++) {
 					for(int k=0; k<y; k++) {
@@ -67,6 +84,7 @@ public class ORMatrix {
 					}
 			}}
 			
+			//result string
 			String result = "";
 		
 			for(int i = 0; i<x; i++) {
@@ -76,9 +94,11 @@ public class ORMatrix {
 				result += "\n";
 			}
 			
+			//add result to resultant array
 			res[j] = result;
 		}
 		
+		//display the results
 		for(int j = 0; j < n; j++)
 			System.out.println(res[j].trim());
 	}
